0c62bc87091a9c732bdd3d1a68bfd5923b96aa12
Charles Siffrein-Blanc <chsiffre@z2r8p6.42lyon.fr>
2023-03-29 18:09:25 +0200

    push to home

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: chsiffre <chsiffre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 17:09:17 by chsiffre          #+#    #+#             */
-/*   Updated: 2023/03/29 15:43:23 by chsiffre         ###   ########.fr       */
+/*   Updated: 2023/03/29 17:51:50 by chsiffre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,7 @@ void	ft_prompt(t_data *data);
 void	ft_builtins(t_data *data);
 void	ft_conv_lst(char *line);
 t_data	ft_init_struct(t_data data);
+void    ft_add_lst(t_lst *lst, t_list *new, char *str);
+t_list	*ft_lstnew_t(void *content, int type);
 
 #endif
--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: chsiffre <chsiffre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 17:08:51 by chsiffre          #+#    #+#             */
-/*   Updated: 2023/03/29 15:47:32 by chsiffre         ###   ########.fr       */
+/*   Updated: 2023/03/29 17:52:03 by chsiffre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,52 @@
 void    ft_parse(t_data *data)
 {
     t_lst   *lst;
-    ssize_t i;
-    ssize_t y;
-    
+    ssize_t  i;
+
     i = -1;
-    y = 0;
     data->result = ft_split_charset(data->line, " \t\n\r\v\f");
     if (!data->result)
         return ;
-    while (data->result[++i])
+    ft_check_redir(data->result, lst);
+}
+
+void    ft_check_redir(char **strs, t_lst *lst)
+{
+    ssize_t i;
+    ssize_t y;
+
+    y = 0;
+    i = -1;
+    while (strs[++i][y] != "|")
     {
-        if (line[i] == "<" || line[i] == ">")
+        if (strs[i][y] == "<" || strs[i][y] == ">")
         {
-            lst = ft_lstnew(line);
-            if (!lst)
-                return ;
-            ft_lstadd_back(lst, line);
-            
-            
+            ft_add_lst(lst, strs[i], 0);
+            i++;
+            ft_add_lst(lst, strs[i], 0);
         }
     }
+}
+
+void    ft_add_lst(t_lst **lst, char *str, int type)
+{
+    t_list  *new;
+    
+    new = ft_lstnew_t(str, type);
+    if (!new)
+        return (-1);
+    ft_lstadd_back(lst, new);
+}
+
+t_list	*ft_lstnew_t(void *content, int type)
+{
+	t_list	*ptr;
+	
+	ptr = malloc(sizeof(t_list));
+	if (!ptr)
+		return (NULL);
+	lst->type = type;
+	ptr->content = content;
+	ptr->next = NULL;
+	return (ptr);
 }
